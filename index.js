633a3dfeb5ca2f3cb98ff039ddfd192d44e62fc7
Thao225 <buithienhuongthao2005@gmail.com>
2024-08-20 23:39:35 +0700

    up

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-window.onload=function(){4
-
+window.onload=function(){
 
   wow = new WOW(
     {
@@ -12,36 +11,76 @@ window.onload=function(){4
   )
   wow.init();
 
+let dangKis=document.querySelectorAll(".noiDungBuocDKs")
+let chons=document.querySelectorAll(".buocDangKis li")
+// for(let c of chons)
+//   c.onclick=function(){
+//     for (let index = 0; index < chons.length; index++) {
+//       if (this == chons[index]) {
+//         // hinhNhan.style=this.style; 
+//         // hinhNhan.style.display = `block`;
+//         dangKis[index].style.display = 'block'
+//       }
+//     }
+//   }
+
+function lapLai(){
+  chons.forEach(li => {
+    li.style.color = 'black';
+  })
+  dangKis[0].innerHTML=dangKis[count+1].innerHTML
+  chons[count].style.color='#Da18DD'
+  count++
+  if (count === chons.length) {
+        count=0
+  }
+}
+
+let count = 0;
+let timerId = setInterval(lapLai, 2000);
+
+for (let i = 0; i < chons.length; i++){
+  chons[i].onclick=function() {
+    chons.forEach(li => {
+      li.style.color = 'black';
+    })
+    dangKis[0].innerHTML=dangKis[i+1].innerHTML
+    chons[i].style.color='#Da18DD'
+    count = i
+  }
+}
+}
+
+// sau 5 giây thì dừng lại
+// setTimeout(() => {
+//   clearInterval(timerId);
+// }, 5000);
 
 // video
-var video = document.querySelector("video"),
-container = document.querySelector("#container");
+// var video = document.querySelector("video"),
+// container = document.querySelector("#container");
 
-var setVideoDimensions = function() {
-var w = video.videoWidth;
-var videoRatio = (w / h).toFixed(2);
+// var setVideoDimensions = function() {
+// var w = video.videoWidth;
+// var videoRatio = (w / h).toFixed(2);
 
-var containerStyles = window.getComputedStyle(container),
-  minW = parseInt(containerStyles.getPropertyValue("width")),
-  minH = parseInt(containerStyles.getPropertyValue("height"));
-var widthRatio = minW / w,
-  heightRatio = minH / h;
-let newWidth = 0;
-let newHeight = 0;
-if (widthRatio > heightRatio) {
-  newWidth = minW;
-  newHeight = Math.ceil(newWidth / videoRatio);
-} else {
-  newHeight = minH;
-  newWidth = Math.ceil(newHeight * videoRatio);
-}
+// var containerStyles = window.getComputedStyle(container),
+//   minW = parseInt(containerStyles.getPropertyValue("width")),
+//   minH = parseInt(containerStyles.getPropertyValue("height"));
+// var widthRatio = minW / w,
+//   heightRatio = minH / h;
+// let newWidth = 0;
+// let newHeight = 0;
+// if (widthRatio > heightRatio) {
+//   newWidth = minW;
+//   newHeight = Math.ceil(newWidth / videoRatio);
+// } else {
+//   newHeight = minH;
+//   newWidth = Math.ceil(newHeight * videoRatio);
+// }
 
-video.style.width = newWidth + "px";
-video.style.height = newHeight + "px";
-};
-video.addEventListener("loadedmetadata", setVideoDimensions, false);
-window.addEventListener("resize", setVideoDimensions, false);
-
-
-//
-}
+// video.style.width = newWidth + "px";
+// video.style.height = newHeight + "px";
+// };
+// video.addEventListener("loadedmetadata", setVideoDimensions, false);
+// window.addEventListener("resize", setVideoDimensions, false);
